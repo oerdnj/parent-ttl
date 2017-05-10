f74e3ad9632dfe22aa3061bb860bc614e65b1f3c
Ondřej Surý <ondrej@sury.org>
2017-05-10 16:59:07 +0200

    Fixup nsd config generation

--- generate-nsd-conf.sh
+++ generate-nsd-conf.sh
@@ -10,18 +10,18 @@ for ttl in no low high max; do
 	    for nstype in in-bailiwick in-domain external; do
 	       	owner="$TAG-$ttl-$amin-$dnssec-$nstype.$ZONE"
 		printf "zone:\n" >> "${NSD_CONF}"
-		printf "\tname: ${owner}\n"
+		printf "\tname: ${owner}\n" >> "${NSD_CONF}"
 		case "${dnssec}" in
 		    dnssec)
-			printf "\tzonefile: ${ZONEDIR}/${owner}.signed" >> "${NSD_CONF}"
-			rm -f "${ZONEDIR}/${owner}.signed"
-			rm -f "${ZONEDIR}/K${owner}*"
-			dnssec-keygen -K "${ZONEDIR}" -a ECDSAP256SHA256 -f KSK -r /dev/urandom "${owner}"
-			dnssec-keygen -K "${ZONEDIR}" -a ECDSAP256SHA256 -r /dev/urandom "${owner}"
-			dnssec-signzone -S -N keep -K "${ZONEDIR}" -o "${owner}" "${ZONEDIR}/${owner}"
+			printf "\tzonefile: ${ZONEDIR}/${owner}.signed\n\n" >> "${NSD_CONF}"
+			rm -f "${ZONEDIR}${owner}.signed"
+			rm -f "${ZONEDIR}K${owner}*"
+			/usr/sbin/dnssec-keygen -v 0 -K "${ZONEDIR}" -a ECDSAP256SHA256 -f KSK -r /dev/urandom "${owner}"
+			/usr/sbin/dnssec-keygen -v 0 -K "${ZONEDIR}" -a ECDSAP256SHA256 -r /dev/urandom "${owner}"
+			/usr/sbin/dnssec-signzone -v 0 -S -N keep -K "${ZONEDIR}" -o "${owner}" "${ZONEDIR}${owner}"
 			;;
 		    nodnssec)
-			printf "\tzonefile: ${ZONEDIR}/${owner}" >> "${NSD_CONF}"
+			printf "\tzonefile: ${ZONEDIR}/${owner}\n\n" >> "${NSD_CONF}"
 			;;
 		esac
 	    done
