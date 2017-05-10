e77353fa3cad2cd66533aa26f2fbc85d0bfea625
Ondřej Surý <ondrej@sury.org>
2017-05-10 16:05:53 +0200

    Improve nsd DS script

--- gather-nsd-keys.sh
+++ gather-nsd-keys.sh
@@ -7,7 +7,7 @@ for ttl in no low high max; do
 	for dnssec in dnssec; do
 	    for nstype in in-bailiwick in-domain external; do
 	       	owner="$TAG-$ttl-$amin-$dnssec-$nstype.$ZONE"
-		dnssec-dsfromkey -2 -f "${ZONEDIR}/${owner}.signed" "${owner}"
+		dnssec-dsfromkey -v 0 -T "${TTL}" -2 -f "${ZONEDIR}/${owner}.signed" "${owner}" 2>/dev/null
 	    done
 	done
     done
