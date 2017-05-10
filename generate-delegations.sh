1a115aa0c4370186fd8cb71af8d2c313d8649049
Ondřej Surý <ondrej@sury.org>
2017-05-10 18:13:53 +0200

    Cleanup cruft

--- generate-delegations.sh
+++ generate-delegations.sh
@@ -7,8 +7,6 @@ for ttl in no low high max; do
 	for dnssec in dnssec nodnssec; do
 	    for nstype in in-bailiwick in-domain external; do
 		owner="$TAG-$ttl-$amin-$dnssec-$nstype.$ZONE"
-		printf "$owner\t$TTL\tIN\tA\t$A\n"
-		printf "$owner\t$TTL\tIN\tAAAA\t$AAAA\n"
 		case $nstype in
 		    in-bailiwick)
 			printf "$owner\t$TTL\tIN\tNS\t${NS1}$TAG-$ttl-$amin-$dnssec-in-domain.$ZONE\n"
